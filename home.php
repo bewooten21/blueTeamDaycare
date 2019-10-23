184897cd844fdb67553f04a86f47a46a8a0941f5
Brad <Brad@10.17.142.17>
2019-10-23 14:35:07 -0500

    basic css

---
+++ home.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include ('css/css.php'); ?>
+        <title></title>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button
+        type="button"
+        class="navbar-toggle collapsed"
+        data-toggle="collapse"
+        data-target="#bs-example-navbar-collapse-1"
+        aria-expanded="false"
+      >
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Balance Web Development</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">About</a></li>
+        <li class="dropdown">
+          <a
+            href="#"
+            class="dropdown-toggle"
+            data-toggle="dropdown"
+            role="button"
+            aria-haspopup="true"
+            aria-expanded="false"
+            >Services<span class="caret"></span
+          ></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Design</a></li>
+            <li><a href="#">Development</a></li>
+            <li><a href="#">Consulting</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+        <div class="jumbotron" >
+            <div class="container"> 
+                <p>yyayyyyyy</p>
+            </div>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once('model/user_db.php');
-require_once('model/user.php');
+
 session_start();
 $action = filter_input(INPUT_POST, 'action');
 if ($action === null) {
@@ -17,122 +16,6 @@ switch ($action) {
         die();
         break;
 
-    case 'contact':
-        include('view/contact.php');
-        die();
-        break;
-    
-    case 'aboutUs':
-        include('view/aboutUs.php');
-        die();
-        break;
-
-    case'register':
-        
-        $emailClass = "";
-        $emailError = "form-group";
-        $unClass = "";
-        $unError = "form-group";
-        $pwClass = "";
-        $pwError = "form-group";
-        $cpwClass = "";
-        $cpwError = "form-group";
-        $fnClass = "";
-        $fnError = "form-group";
-        $lnClass = "";
-        $lnError = "form-group";
-
-        $email_error = '';
-        $un_error = '';
-        $pw_error = '';
-        $cpw_error = '';
-        $fn_error = '';
-        $ln_error = '';
-        $email = '';
-        $un = '';
-        $pw = '';
-        $cpw = '';
-        $fn = '';
-        $ln = '';
-        include('view/register.php');
-        die();
-        break;
-
-    case 'valRegister':
-        include('model/valRegister.php');
-        die();
-        break;
-    case 'login':
-        $un = '';
-        $pw = '';
-        $un_error = '';
-        $pw_error = '';
-        $unClass = "";
-        $unError = "form-group";
-        $pwClass = "";
-        $pwError = "form-group";
-        include('view/login.php');
-        die();
-        break;
-    case 'valLogin':
-        include('model/valLogin.php');
-        die();
-        break;
-    case 'logout':
-        session_destroy();
-        unset($_SESSION['username']);
-       header("Location: home.php");
-
-    case 'forum':
-        include('view/forum.php');
-        die();
-        break;
-    
-     case 'viewAccount':
-        include('view/account.php');
-        die();
-        break;
-    case 'accountInfo':
-       $emailClass = "";
-        $emailError = "form-group";
-        $unClass = "";
-        $unError = "form-group";
-        
-        $fnClass = "";
-        $fnError = "form-group";
-        $lnClass = "";
-        $lnError = "form-group";
-
-        $email_error = '';
-        $un_error = '';
-        
-        $fn_error = '';
-        $ln_error = '';
-        $email = '';
-        $un = '';
-        $pw = '';
-        $cpw = '';
-        $fn = '';
-        $ln = '';
-        include('view/accountInfo.php');
-        die();
-        break;
-    
-    case 'resetPw':
-        $pw="";
-        $pwClass = "";
-        $pwError = "form-group";
-        $cpwClass = "";
-        $cpwError = "form-group";
-        $pw_error = '';
-        $cpw_error = '';
-        include('view/resetPw.php');
-        die();
-        break;
-    case 'valUpdateUser':
-        include('model/valUpdateUser.php');
-        die();
-        break;
         
 }
      
