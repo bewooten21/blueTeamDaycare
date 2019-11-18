7932af19937e5b9a96702f8101fabf26281b21d3
ts526610 <ts526610@LR00110.southeast.edu>
2019-11-18 16:20:30 -0600

    Needed to redo a bad merge. Committing files from outside the merge conflicts.

---
+++ views/editChild.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Add Child</title>
+        <?php include ('css/css.php');  ?> 
+    </head>
+    <body>
+        <?php include('nav.php'); ?> 
+        <div class="container">
+  <h2>Add Child</h2>
+<form class="form-horizontal" method="post">
+    <input type="hidden" name="action" value="addStuVal">
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="fName">Child First Name:</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" id="fName" name="fName" placeholder="Enter first name" value="<?php echo htmlspecialchars($child["stuFName"]); ?>">
+     </div>
+    <div class="col-sm-2">
+          <p class="error">
+              <?php echo $fNError; ?>
+          </p>
+      </div>
+    
+  </div>
+  <div class="form-group">
+    <label class="control-label col-sm-2" for="jobTitle">Child Last Name:</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" id="lName" name="lName" placeholder="Enter last name" value="<?php echo htmlspecialchars($child["stuLName"]); ?>">
+    </div>
+    <div class="col-sm-2">
+          <p class="error">
+              <?php echo $lNError; ?>
+          </p>
+      </div>
+  </div>
+    
+    <div class="form-group">
+    <label class="control-label col-sm-2" for="jobR">Child Age:</label>
+    <div class="col-sm-4">
+      <input type="text" class="form-control" id="age" name="age" placeholder="Enter age" value="<?php echo htmlspecialchars($child["age"]); ?>">
+    </div>
+    <div class="col-sm-2">
+          <p class="error">
+              <?php echo $ageError; ?>
+          </p>
+      </div>
+  </div>
+  
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Submit</button>
+    </div>
+  </div>
+</form>
+      
+        
+    </body>
+</html>
+
---
+++ views/review.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>DeDiSystems</title>
+  <?php include ('css/css.php'); ?> 
+</head>
+<body>
+<?php include('nav.php') ?>
+    <form action="index.php" method="post">
+        <lable>Feedback</lable></br>
+        <input type="textarea" class="form-control" name="feedback">
+        <label>Rating</label></br>
+        <input type="text" class="form-control" name="rating">
+        <input type="hidden" name="action" value="submitFeedback">
+        <input type="submit" value="submit">
+</body>
+</html>
+
+
