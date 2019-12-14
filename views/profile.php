c97c63182fd989ac5a49d879f39969e9acaf5b51
Brad <Brad@10.17.155.163>
2019-12-14 16:58:06 -0600

    style buttons

--- views/profile.php
+++ views/profile.php
@@ -10,69 +10,71 @@
         <main>
             <div class="container">
                 <h1>Welcome to your profile page <?php echo htmlspecialchars($_SESSION['currentUser']->getFName()); ?>!</h1>
-                <?php if ((int)$_SESSION['currentUser']->getRestricted() === 1) { ?> <h2> Your account is currently restricted pending administrator review!</h2> <?php } ?>
+                <?php if ((int) $_SESSION['currentUser']->getRestricted() === 1) { ?> <h2> Your account is currently restricted pending administrator review!</h2> <?php } ?>
                 <div id="profileImg">
                     <p><img src="<?php echo htmlspecialchars($_SESSION['currentUser']->getImage()); ?>" width="200"></p>
                 </div>
                 <h3><?php echo htmlspecialchars($_SESSION['currentUser']->getUName()); ?></h3> 
                 <p>Email: <?php echo htmlspecialchars($_SESSION['currentUser']->getEmail()); ?></p></br>
-                <h2>Children</h2>
-                <?php if (isset($_SESSION['currentUser'])) : if($_SESSION['currentUser']->getRestricted() != 1) :  ?>
-                <a href='index.php?action=addStudent'>Add child/student</a>
-                <?php if($children!=false){ ?>
-                <form  method="post">
-                    <input type="hidden" name="action" value="editChild">
-                <select id="stuId" name="stuId">
-                                    
-                   
-                                  <?php foreach ($children as $c) : ?>
-                                    <option value="<?php echo $c['studentId'] ?>"> <?php echo $c['stuFName']. " " . $c["stuLName"]. "  " . "Age:" . $c["age"];?> </option>
-                                    <?php endforeach; ?>  
-                                    
-                                </select>
-               
-                    
-                    <input type="submit" class="btn btn-primary btn-sml" value="Edit Child"><br>
-                </form>
-                   <?php } endif;
-                   endif;?><br>
-                   
-                   
-                
-                
-                <table class="table table-dark">
-                    <tr>
-                        <th> Comments</th>
-                    </tr>
-                    <?php foreach ($comments as $single) : ?>
+                <?php if (isset($_SESSION['currentUser']) && $_SESSION['currentUser']->getRestricted() != 1) { ?>
+
+                    <h2>Children</h2>
+
+                    <a href='index.php?action=addStudent'>Add child/student</a>
+                    <?php if ($children != false) { ?>
+                        <form  method="post">
+                            <input type="hidden" name="action" value="editChild">
+                            <select id="stuId" name="stuId">
+
+
+                                <?php foreach ($children as $c) : ?>
+                                    <option value="<?php echo $c['studentId'] ?>"> <?php echo $c['stuFName'] . " " . $c["stuLName"] . "  " . "Age:" . $c["age"]; ?> </option>
+                                <?php endforeach; ?>  
+
+                            </select>
+
+
+                            <input type="submit" class="btn btn-primary btn-sml" value="Edit Child"><br>
+                        </form><br>
+                    <?php }; ?>
+
+
+
+
+
+                    <table class="table table-dark">
                         <tr>
-                            <td>
-                                <strong><?php echo htmlspecialchars($single->getCommenterName() . ' said:'); ?></strong><br>
-                                <i><?php echo htmlspecialchars($single->getComment()) ?></i><br> 
-                                <?php echo  htmlspecialchars(' at ' . $single->getCommentTime()); ?>
-                            </td>
+                            <th> Comments</th>
                         </tr>
-                    <?php endforeach; ?>
-                </table><br>
-                <?php if (isset($_SESSION['currentUser'])) : if($_SESSION['currentUser']->getRestricted() != 1) :  ?>
-                <div class="row">
-                    <div class="col-sm-2">
-                <form action="index.php" method="post">
-                    <input type="hidden" name="action" value="changeInfo">
-                    <input type="submit" class="btn btn-primary btn-sml" value="Change Profile Info"><br>
-                </form>
+                        <?php foreach ($comments as $single) : ?>
+                            <tr>
+                                <td>
+                                    <strong><?php echo htmlspecialchars($single->getCommenterName() . ' said:'); ?></strong><br>
+                                    <i><?php echo htmlspecialchars($single->getComment()) ?></i><br> 
+                                    <?php echo htmlspecialchars(' at ' . $single->getCommentTime()); ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </table><br>
+
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <form action="index.php" method="post">
+                                <input type="hidden" name="action" value="changeInfo">
+                                <input type="submit" class="btn btn-primary btn-sml" value="Change Profile Info"><br>
+                            </form>
                         </div>
-                    <?php if($userCompany == false) : ?>
-                    <div class="col-sm-2">
-                <form action="index.php" method="post">
-                    <input type="hidden" name="action" value="registerBusiness">
-                    <input type="submit" class="btn btn-primary btn-sml" value="Register Business"><br>
-                </form>
+                        <?php if ($userCompany == false) : ?>
+                            <div class="col-sm-2">
+                                <form action="index.php" method="post">
+                                    <input type="hidden" name="action" value="registerBusiness">
+                                    <input type="submit" class="btn btn-primary btn-sml" value="Register Business"><br>
+                                </form>
+                            </div>
+                        <?php endif; ?>
                     </div>
-                    <?php endif; ?>
-                    </div>
-                <?php endif;
-                endif;?>
+                <?php } ?>
+
             </div>
         </main>
         <?php include ('footer.php'); ?>
